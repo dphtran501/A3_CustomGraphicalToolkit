b7f899e68f5576af6fc1817eec514e0fcc4d84c3
Derek Tran <dphtran501@gmail.com>
2021-05-14 02:16:40 -0700

    Add checkbox widget

--- demo.js
+++ demo.js
@@ -19,3 +19,26 @@ btn.onExecute(e => {
 btn.onClick(function(e){
 	console.log("CLICK: ", e);
 });
+
+// Implement a MyToolkit Checkbox
+var chkbox = new MyToolkit.CheckBox;
+chkbox.setText("This is a test.")
+chkbox.move(100, 200);
+chkbox.onIdle(e => {
+	console.log("IDLE: ", e)
+});
+chkbox.onIdleHover(e => {
+	console.log("IDLE HOVER: ", e)
+});
+chkbox.onPressedDown(e => {
+	console.log("PRESSED DOWN: ", e)
+});
+chkbox.onExecute(e => {
+	console.log("EXECUTE: ", e)
+});
+chkbox.onCheck(e => {
+	console.log("CHECK: ", e)
+});
+chkbox.onUncheck(e => {
+	console.log("UNCHECK: ", e)
+});
--- toolkit.js
+++ toolkit.js
@@ -1,5 +1,9 @@
 let MyToolkit = (function() {
 
+    let defaultFontSize = 16;
+    let defaultFontWeight = 400;
+    let defaultFontFamily = 'sans-serif'
+
     let draw = SVG().addTo('body').size('1000px','1000px');
 
     let Button = function(){
@@ -9,6 +13,7 @@ let MyToolkit = (function() {
         let buttonLeftPadding = 10;
         let buttonRightPadding = 10;
         let buttonZ = 8;   // "z" height of button to make it pop out of canvas
+        let buttonCornerRadius = 10;
         let gradient = draw.gradient('radial', add => {
             add.stop(0, '#999999');
             add.stop(1, '#666666');
@@ -24,7 +29,7 @@ let MyToolkit = (function() {
             "PRESSED_DOWN": 3,
             "EXECUTE": 4
         }
-        var currentState = buttonState.IDLE_UP;
+        let currentState = buttonState.IDLE_UP;
 
         // Construct button
         let button = draw.group();
@@ -32,25 +37,25 @@ let MyToolkit = (function() {
         let rectTopGroup = button.group();
         let rectTop = rectTopGroup.rect(buttonWidthDefault, buttonHeightDefault)
             .fill(gradient)
-            .radius(10);
+            .radius(buttonCornerRadius);
         let buttonText = rectTopGroup.text("Button");
         buttonText.font({
-            size: 16,
-            weight: 400,
-            family: "sans-serif",
+            size: defaultFontSize,
+            weight: defaultFontWeight,
+            family: defaultFontFamily,
             fill: 'white'
         });
         centerButtonText(0, 0); // rect starts at 0,0
 
         let rectSide = button.rect(buttonWidthDefault, buttonHeightDefault)
             .fill('#111111')
-            .radius(10)
+            .radius(buttonCornerRadius)
             .dy(buttonZ);
         rectSide.insertBefore(rectTop);
 
         // Mask entire button so event handlers triggered on entire button rather than parts of button
         let buttonMask = button.rect(buttonWidthDefault, buttonHeightDefault + buttonZ)
-            .radius(10)
+            .radius(buttonCornerRadius)
             .opacity(0);
         buttonMask.front();
 
@@ -162,8 +167,159 @@ let MyToolkit = (function() {
             }
         }
     }
+
+    let CheckBox = function() {
+
+        let checkboxLength = 20;
+        let checkboxCornerRadius = 4;
+        let textIndent = 40;
+
+        const checkboxState = {
+            "IDLE": 1,
+            "IDLE_HOVER": 2,
+            "PRESSED_DOWN": 3,
+            "EXECUTE": 4
+        }
+        var currentState = checkboxState.IDLE;
+        
+        let isChecked = false;
+
+        // Construct checkbox
+        let checkbox = draw.group();
+
+        let boxGroup = checkbox.group();
+        let box = boxGroup.rect(checkboxLength, checkboxLength)
+            .stroke({color: '#444444', width: 2})
+            .fill({color: '#C5E8B7', opacity: 0})
+            .radius(checkboxCornerRadius);
+
+        let checkmark = boxGroup.polyline('0,8 4,12 12,0')
+            .fill('none')
+            .stroke({
+                color: '#2EB62C',
+                width: 4
+            })
+            .center(box.cx(), box.cy());
+        showCheckmark(isChecked);   // Default should be unchecked
+
+        let checkboxText = checkbox.text("Item")
+            .font({
+                size: defaultFontSize,
+                weight: defaultFontWeight,
+                family: defaultFontFamily,
+            })
+        checkboxText.move(textIndent, (checkboxLength - checkboxText.font('size')) / 2);    // Vertically center with checkbox
+
+        // Mask entire checkbox (excluding text) so event handlers triggered on entire box rather than parts of box
+        let boxMask = boxGroup.rect(checkboxLength, checkboxLength)
+            .radius(checkboxCornerRadius)
+            .opacity(0);
+        boxMask.front();
+
+        // Checkbox event handling
+        let idleEventHandler = null;
+        let idleHoverEventHandler = null;
+        let pressedDownEventHandler = null;
+        let executeEventHandler = null;
+        let checkEventHandler = null;
+        let uncheckEventHandler = null;
+
+        boxMask.mouseover((event) => {
+            if (currentState != checkboxState.IDLE_HOVER) {
+                currentState = checkboxState.IDLE_HOVER;
+                box.stroke({width: 4});
+                boxGroup.css('cursor', 'pointer');
+                if(idleHoverEventHandler != null) {
+                    idleHoverEventHandler(event);
+                }
+            }          
+        });
+        boxMask.mouseout((event) => {
+            if (currentState == checkboxState.PRESSED_DOWN) {
+                boxMask.fire('mouseup');
+            }
+            if (currentState != checkboxState.IDLE) {
+                currentState = checkboxState.IDLE;
+                box.stroke({width: 2});
+                if(idleEventHandler != null) {
+                    idleEventHandler(event);
+                }
+            }
+        });
+        boxMask.mousedown((event) => {
+            if (currentState != checkboxState.PRESSED_DOWN) {
+                currentState = checkboxState.PRESSED_DOWN;
+                box.fill({color: '#C5E8B7', opacity: 0.5})
+                if (pressedDownEventHandler != null) {
+                    pressedDownEventHandler(event);
+                }
+            }
+        });
+        boxMask.mouseup(() => {
+            if (currentState == checkboxState.PRESSED_DOWN) {
+                box.fill({color: '#C5E8B7', opacity: 0});
+            }
+        });
+        boxMask.click(event => {
+            isChecked = !isChecked;
+            showCheckmark(isChecked);
+            if (isChecked && checkEventHandler != null) {
+                checkEventHandler(event);
+            } else if (!isChecked && uncheckEventHandler != null) {
+                uncheckEventHandler(event);
+            }
+            currentState == checkboxState.EXECUTE;
+            if (executeEventHandler != null) {
+                executeEventHandler(event);
+            }
+            boxMask.fire('mouseover');
+        });
+
+        function showCheckmark(isChecked) {
+            if (isChecked) {
+                checkmark.opacity(1);
+            } else {
+                checkmark.opacity(0);
+            }
+        }
+
+        return {
+            move: function(x, y) {
+                checkbox.move(x, y);
+            },
+
+            onIdle: function(eventHandler) {
+                idleEventHandler = eventHandler;
+            },
+
+            onIdleHover: function(eventHandler) {
+                idleHoverEventHandler = eventHandler;
+            },
+
+            onPressedDown: function(eventHandler) {
+                pressedDownEventHandler = eventHandler;
+            },
+
+            onExecute: function(eventHandler) {
+                executeEventHandler = eventHandler;
+            },
+
+            onCheck: function(eventHandler) {
+                checkEventHandler = eventHandler;
+            },
+
+            onUncheck: function(eventHandler) {
+                uncheckEventHandler = eventHandler;
+            },
+
+            setText: function(text) {
+                checkboxText.clear();
+                checkboxText.text(text);
+            }
+        }
+    }
     
-    return {Button}
+    return {Button, CheckBox}
 
 }());
 
